7a49a082888aff01a9dcd9fc2b8c42e6b5ccc4c2
S. Makai Takori <makait87@gmail.com>
2019-12-17 17:03:10 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,25 +4,22 @@ def line(array)
   line_string = "The line is currently:"
   
   if array.length == 0  
-   puts "The line is currently empty." 
+  puts "The line is currently empty." 
   else 
-      array.each_with_index {|name, i|
+      array.each.with_index(1) {|name, i|
     line_string += " #{i}. #{name}"  
   } 
   puts line_string 
-    
-end
+  end
 end  
 
 def take_a_number(katz_deli,name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-  
 end
-  
 
 def now_serving(array)
-  if array.length = = 0
+  if array.length == 0
     puts "There is nobody waiting to be served!"
   else   
     puts "Currently serving #{array[0]}."
@@ -30,4 +27,3 @@ def now_serving(array)
   end  
 end  
 
-puts line(["Makai","Alfredo"])
