fae912cfaf6617003efa06a2244658c28252b3e5
S. Makai Takori <makait87@gmail.com>
2019-12-17 03:39:00 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(katz_deli)
+  katz_deli = []
+  katz_deli.length == 0  
+    puts "The line is currently empty." 
+end  
+
+def take_a_number(katz_deli,name)
+  katz_deli = []
+  katz_deli.each_with_index{||}
+  
+  katz_deli << name
+    puts  
+  
+
+end  
